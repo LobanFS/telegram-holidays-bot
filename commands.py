35ecbfa761aeb966b3ace7a4c2d2dafe7ace58cb
Philipp <lapsha53@gmail.com>
2025-01-26 19:08:18 +0300

    добавил комманду /holidays

---
+++ commands.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.devtools.v85.web_audio import ContextType
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, CallbackContext
+from holidays_parser import get_today_holidays
+
+async def holidays(update: Update):
+    holidays_list = get_today_holidays()
+    if holidays_list:
+        response = "Сегодня:\n" + "\n".join(holidays_list)
+    else:
+        response = "Проблемы с парсингом или доступом или еще чем-то :(."
+    await update.message.reply_text(response)
+
--- main.py
+++ main.py
@@ -1,3 +1,1 @@
 from holidays_parser import get_today_holidays
-
-print(get_today_holidays())
