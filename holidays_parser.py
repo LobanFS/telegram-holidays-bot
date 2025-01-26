62995538631cdbe6a2d733c35eb6cbb038a32edd
Philipp <lapsha53@gmail.com>
2025-01-26 18:42:43 +0300

    обезопасил парсер, изменил reqs

--- holidays_parser.py
+++ holidays_parser.py
@@ -7,12 +7,18 @@ from bs4 import BeautifulSoup
 def get_today_holidays():
     url = "https://kakoysegodnyaprazdnik.ru/"
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get(url)
-    driver.implicitly_wait(1)
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'html.parser')
-    holidays = []
-    for holiday in soup.select('[itemprop="text"]'):
-        holidays.append(holiday.get_text(strip=True))
-    return holidays
+    try:
+        driver.get(url)
+        driver.implicitly_wait(1)
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+        holidays = []
+        for holiday in soup.select('[itemprop="text"]'):
+            holidays.append(holiday.get_text(strip=True))
+        return holidays
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+    finally:
+        driver.quit()
 
