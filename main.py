f9277f5e9fdea766d15f05237ad85dfe43f8c312
Philipp <lapsha53@gmail.com>
2025-01-26 19:40:21 +0300

    обновлен main, commands

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import os
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import Updater, CommandHandler
 from holidays_parser import get_today_holidays
+from commands import *
+
+load_dotenv()
+def main():
+    token = os.getenv("TELEGRAM_TOKEN")
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+
+    #Обработка команд
+    dispatcher.add_handler(CommandHandler("holidays", holidays))
+
+    #Запуск
+    updater.start_polling()
+    updater.idle()
+
+
+
+
+
